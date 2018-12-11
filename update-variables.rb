396b3e9387553afa47eb102c77004cfc2a685d72
Andrej Petras <andrej@lorislab.org>
2018-12-11 22:54:36 +0100

    Create travis configuration script.

--- update-variables.rb
+++ update-variables.rb
@@ -1,4 +1,5 @@
 require 'travis'
+Travis::Pro.access_token = ENV['TRAVIS_TOKEN']
 repos = Travis::Pro::Repository.find_all(owner_name: 'lorislab')
 	.reject{|repo| repo.slug == 'lorislab/travis-configuration'}
 	.select{|repo| Travis::Pro::User.current.admin_access?(repo)}
@@ -7,7 +8,8 @@ repos.each do |repo|
     tmp_repo = Travis::Pro::Repository.find(repo.slug)
     nv_vars = tmp_repo.env_vars
 	keys.each do |key|
-		puts "Setting env var '#{key}' on project '#{repo.slug}' #{tmp_repo.last_build_number}"
+		puts "Setting env var '#{key}' on project '#{repo.slug}' #{tmp_repo.last_build_number
+		}"
 		nv_vars.upsert(key, "'#{ENV[key]}'", public: false)
 	end
 end
