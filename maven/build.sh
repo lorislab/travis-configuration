1553a0bc3f22545537bfe1b1071d9f269620f2a0
Andrej Petras <andrej@lorislab.org>
2018-12-12 00:27:55 +0100

    Create travis configuration script.

--- maven/build.sh
+++ maven/build.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-mvn -s .scripts/maven/settings.xml clean install
+# master
+if [[ $TRAVIS_BRANCH = master && $TRAVIS_PULL_REQUEST = false ]]; then
+    mvn -s .scripts/maven/settings.xml clean install
+    mvn -s .scripts/maven/settings.xml deploy
+fi
+
+# develop
+if [[ $TRAVIS_BRANCH = develop && $TRAVIS_PULL_REQUEST = false ]]; then
+    mvn -s .scripts/maven/settings.xml clean install
+    mvn -s .scripts/maven/settings.xml deploy
+fi
+
+# other
+if [[ $TRAVIS_BRANCH != master && $TRAVIS_BRANCH != develop && $TRAVIS_PULL_REQUEST = false ]]; then
+    mvn -s .scripts/maven/settings.xml clean install
+fi
