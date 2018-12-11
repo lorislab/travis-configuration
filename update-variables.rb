41b90a0ee16c3ae3eb37e4318a2898fdfb9c8838
Andrej Petras <andrej@lorislab.org>
2018-12-11 23:14:48 +0100

    Create travis configuration script.

--- update-variables.rb
+++ update-variables.rb
@@ -3,6 +3,7 @@ Travis::Pro.access_token = ENV['TRAVIS_TOKEN']
 repos = Travis::Pro::Repository.find_all(member: Travis::Pro::User.current.login)
 	.reject{|repo| repo.slug == 'lorislab/travis-configuration'}
 	.select{|repo| Travis::Pro::User.current.admin_access?(repo)}
+	.select{|repo| repo.active }
 keys = ['BINTRAY_API_KEY']
 
 #tmp_repo = Travis::Pro::Repository.find('lorislab/treasure')
