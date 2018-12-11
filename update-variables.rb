9f332d85f07cbb8027abf150bd5eb8f318186ed2
Andrej Petras <andrej@lorislab.org>
2018-12-11 23:24:54 +0100

    Create travis configuration script.

--- update-variables.rb
+++ update-variables.rb
@@ -3,20 +3,14 @@ Travis::Pro.access_token = ENV['TRAVIS_TOKEN']
 repos = Travis::Pro::Repository.find_all(member: Travis::Pro::User.current.login)
 	.reject{|repo| repo.slug == 'lorislab/travis-configuration'}
 	.select{|repo| Travis::Pro::User.current.admin_access?(repo)}
-	.select{|repo| repo.active? }
-keys = ['BINTRAY_API_KEY']
 
-#tmp_repo = Travis::Pro::Repository.find('lorislab/treasure')
-#nv_vars = tmp_repo.env_vars
-#keys.each do |key|
-#    nv_vars.upsert(key, "'#{ENV[key]}'", public: false)
-#end
+keys = ['BINTRAY_API_KEY']
 
 repos.each do |repo|
     tmp_repo = repo
     nv_vars = tmp_repo.env_vars
 	keys.each do |key|
-		puts "Setting env var '#{key}' on project '#{repo.slug}' #{tmp_repo.last_build_number}"
+		puts "Setting env var '#{key}' on project '#{repo.slug}'"
 		nv_vars.upsert(key, "'#{ENV[key]}'", public: false)
 	end
 end
