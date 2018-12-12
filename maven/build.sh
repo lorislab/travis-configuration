59cbdeeb0434f716cf8fe7c297c8355cb291062f
Andrej Petras <andrej@lorislab.org>
2018-12-12 01:19:33 +0100

    Create travis configuration script.

--- maven/build.sh
+++ maven/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-export VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+export VERSION=`$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)`
 # change version
 if [[ $TRAVIS_BRANCH = master && $TRAVIS_PULL_REQUEST = false ]]; then
     export VERSION=${VERSION%%-*}
