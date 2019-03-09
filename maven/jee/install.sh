cf0ee61ccde1060e7c7f710acaaab6516cee479f
Andrej Petras <andrej@lorislab.org>
2019-03-09 20:17:40 +0100

    Add the scripts for the test-containers example.

--- maven/jee/install.sh
+++ maven/jee/install.sh
@@ -3,4 +3,4 @@ set -e
 
 export MAVEN_CLI_OPTS="-s .scripts/maven/settings.xml --batch-mode --errors --fail-at-end --show-version"
 
-mvn $MAVEN_CLI_OPTS -DskipTests=true -Dmaven.javadoc.skip=true -B
+mvn $MAVEN_CLI_OPTS -DskipTests=true -Dmaven.javadoc.skip=true -B dependency:resolve
