76b365c83bf916b9f907fb5ded238fd7e228954c
Andrej Petras <andrej@lorislab.org>
2019-03-10 17:01:23 +0100

    Add the scripts for the test-containers example.

--- maven/jee/docker_deploy.sh
+++ maven/jee/docker_deploy.sh
@@ -4,6 +4,7 @@ set -e
 export MAVEN_CLI_OPTS="-s .scripts/maven/settings.xml --batch-mode --errors --fail-at-end --show-version"
 
 # deploy
+echo $BUILD_VERSION
 
 if [[ $TRAVIS_PULL_REQUEST = false ]]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
@@ -13,5 +14,5 @@ if [[ $TRAVIS_PULL_REQUEST = false ]]; then
         export BUILD_VERSION=$(mvn help:evaluate -N -Dexpression=project.version|grep -v '\[')
         DOCKER_TAG=${BUILD_VERSION%%-*}
     fi
-    mvn $MAVEN_CLI_OPTS -Pdocker-push -Ddockerfile.tag=$DOCKER_TAG dockerfile:push
+    mvn $MAVEN_CLI_OPTS -Ddockerfile.tag=$DOCKER_TAG dockerfile:push
 fi
